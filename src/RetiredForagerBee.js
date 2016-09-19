44e66fe87d633161ba8fa7f6cb346d21a106c6f5
mjstromberg <michaeljstromberg@gmail.com>
2016-09-19 11:51:35 -0700

    Implement RetiredForagerBee.js.

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,7 +1,19 @@
 import ForagerBee from './ForagerBee';
 
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee{
+  constructor(food) {
+  	super(food);
+  	this.age = 40;
+  	this.job = 'gamble';
+  	this.canFly = false;
+  	this.color = 'grey';
+  }
+  forage() {
+  	return 'I am too old, let me play cards instead';
+  }
+  gamble(treasure) {
+  	this.treasureChest.push(treasure);
+  }
 };
 
 export default RetiredForagerBee;
