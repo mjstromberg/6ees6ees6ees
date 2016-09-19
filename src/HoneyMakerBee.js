c9f458dbe2b111350e663b76411dafecec8ea297
mjstromberg <michaeljstromberg@gmail.com>
2016-09-19 11:50:36 -0700

    Implement HoneyMakerBee.js.

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,7 +1,18 @@
 import Bee from './Bee'
 
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee{
+  constructor(color) {
+  	super(color);
+  	this.age = 10;
+  	this.job = 'make honey';
+  	this.honeyPot = 0;
+  }
+  makeHoney() {
+  	this.honeyPot++;
+  }
+  giveHoney() {
+  	this.honeyPot--;
+  }
 };
 
 export default HoneyMakerBee;
