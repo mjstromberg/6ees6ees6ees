c3aa48b5feec846a79a8f4d1277a60ab6a9896f5
mjstromberg <michaeljstromberg@gmail.com>
2016-09-19 11:38:13 -0700

    Implement Bee.js.

--- src/Bee.js
+++ src/Bee.js
@@ -1,7 +1,12 @@
 import Grub from './Grub'
 
-class Bee {
-  // TODO..
+class Bee extends Grub{
+  constructor(food) {
+  	super(food);
+  	this.age = 5;
+  	this.color = 'yellow';
+  	this.job = 'Keep on growing';
+  }
 };
 
 export default Bee;
